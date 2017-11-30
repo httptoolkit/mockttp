b827fb02202a8b8dcf7e8b83a43242530f18e59f
Tim Perry <pimterry@gmail.com>
2017-11-30 15:13:41 +0100

    Set up wallaby with the latest testing changes

--- wallaby.js
+++ wallaby.js
@@ -1,21 +1,34 @@
 module.exports = (wallaby) => {
+  process.env.NODE_EXTRA_CA_CERTS = './test/fixtures/test-ca.pem'
+
   return {
     files: [
       'package.json',
       'src/**/*.ts',
-      'test/*.ts'
+      'src/**/*.gql',
+      'test/**/*.ts',
+      'test/fixtures/**/*',
+      '!test/**/*-test.ts'
     ],
-
     tests: [
       'test/**/*-test.ts'
     ],
 
-    env: {
-      type: 'node'
+    preprocessors: {
+      // Package.json points `main` to the built output. We use this a lot in the integration tests, but we
+      // want wallaby to run on raw source. This is a simple remap of paths to lets us do that.
+      'test/integration/*.ts': file => {
+        return file.content.replace(
+        /("|')..\/..("|')/g,
+        '"../../src/main"'
+      );
+    }
     },
 
     testFramework: 'mocha',
-
-    debug: false
+    env: {
+      type: 'node'
+    },
+    debug: true
   };
 };
