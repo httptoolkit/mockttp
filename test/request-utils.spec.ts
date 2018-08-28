e1a856b37dbafee5e9fa14a89be24af73f3b00c9
Tim Perry <pimterry@gmail.com>
2018-08-28 19:42:35 +0200

    Add standalone unit tests for body decoding

---
+++ test/request-utils.spec.ts
@@ -0,0 +1,51 @@
+import * as zlib from 'zlib';
+
+import { expect } from './test-utils';
+import { buildBodyReader } from '../src/server/request-utils';
+
+describe("buildBodyReader", () => {
+    describe(".text", () => {
+        it('returns the raw text for unspecified requests', () => {
+            const body = buildBodyReader(Buffer.from('hello world'), {});
+            expect(body.text).to.equal('hello world');
+        });
+
+        it('returns the raw text for identity requests', () => {
+            const body = buildBodyReader(Buffer.from('hello world'), {
+                'content-encoding': 'identity'
+            });
+            expect(body.text).to.equal('hello world');
+        });
+
+        it('is undefined for unknown encodings', () => {
+            const body = buildBodyReader(Buffer.from('hello world'), {
+                'content-encoding': 'randomized'
+            });
+            expect(body.text).to.equal(undefined);
+        });
+
+        it('can decode gzip bodies', () => {
+            const content = Buffer.from(zlib.gzipSync('Gzip response'));
+            const body = buildBodyReader(content, {
+                'content-encoding': 'gzip'
+            });
+            expect(body.text).to.equal('Gzip response');
+        });
+
+        it('can decode zlib deflate bodies', () => {
+            const content = Buffer.from(zlib.deflateSync('Deflate response'));
+            const body = buildBodyReader(content, {
+                'content-encoding': 'deflate'
+            });
+            expect(body.text).to.equal('Deflate response');
+        });
+
+        it('can decode raw deflate bodies', () => {
+            const content = Buffer.from(zlib.deflateRawSync('Raw deflate response'));
+            const body = buildBodyReader(content, {
+                'content-encoding': 'deflate'
+            });
+            expect(body.text).to.equal('Raw deflate response');
+        });
+    });
+});
