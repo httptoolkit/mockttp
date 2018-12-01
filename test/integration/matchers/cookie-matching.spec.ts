c4312eb28d80e5a613112f887844a63cb76d9247
Tim Perry <pimterry@gmail.com>
2018-12-01 01:05:06 +0100

    Simplify cookie matcher testing code

--- test/integration/matchers/cookie-matching.spec.ts
+++ test/integration/matchers/cookie-matching.spec.ts
@@ -16,33 +16,20 @@ nodeOnly(() => {
         });
 
         it("should match requests with the matching cookie", async () => {
-            let response: any = {};
-            let status: any = null;
             const requestCookie = request.cookie('supercookie=yummi');
             const cookiejar = request.jar();
 
             cookiejar.setCookie(requestCookie, server.url);
 
-            const options = {
+            const response = await request({
                 uri: server.url,
-                jar: cookiejar,
-                resolveWithFullResponse: true,
-                simple: false,
-            };
+                jar: cookiejar
+            });
 
-
-            await request(options)
-                .then((resp) => {
-                    response = resp.body;
-                    status = resp.statusCode;
-                });
-
-            expect(await response).to.equal("matched cookie");
+            expect(response).to.equal("matched cookie");
         });
 
         it("should match requests with the matching cookie when multiple cookies are present", async () => {
-            let response: any = {};
-            let status: any = null;
             const matchingCookie = request.cookie('supercookie=yummi');
             const notMatchingCookie = request.cookie('megacookie=delicious');
             const cookiejar = request.jar();
@@ -50,63 +37,38 @@ nodeOnly(() => {
             cookiejar.setCookie(matchingCookie, server.url);
             cookiejar.setCookie(notMatchingCookie, server.url);
 
-            const options = {
+            const response = await request({
                 uri: server.url,
-                jar: cookiejar,
-                resolveWithFullResponse: true,
-                simple: false,
-            };
+                jar: cookiejar
+            });
 
-
-            await request(options)
-                .then((resp) => {
-                    response = resp.body;
-                    status = resp.statusCode;
-                });
-
-            expect(await response).to.equal("matched cookie");
+            expect(response).to.equal("matched cookie");
         });
 
         it("should not match requests when cookie has a different value", async () => {
-            let response: any = {};
-            let status: any = null;
             const requestCookie = request.cookie('megacookie=delicious; supercookie=delicious');
             const cookiejar = request.jar();
 
             cookiejar.setCookie(requestCookie, server.url);
 
-            const options = {
+            const { statusCode } = await request({
                 uri: server.url,
                 jar: cookiejar,
                 resolveWithFullResponse: true,
-                simple: false,
-            };
+                simple: false
+            })
 
-            await request(options)
-                .then((resp) => {
-                    response = resp.body;
-                    status = resp.statusCode;
-                });
-
-            expect(status).to.equal(503);
+            expect(statusCode).to.equal(503);
         });
 
         it("should not match requests when no cookies are present", async () => {
-            let response: any = {};
-            let status: any = null;
-            const options = {
+            const { statusCode } = await request({
                 uri: server.url,
                 resolveWithFullResponse: true,
-                simple: false,
-            };
+                simple: false
+            })
 
-            await request(options)
-                .then((resp) => {
-                    response = resp.body;
-                    status = resp.statusCode;
-                });
-
-            expect(status).to.equal(503);
+            expect(statusCode).to.equal(503);
         });
     });
 });
