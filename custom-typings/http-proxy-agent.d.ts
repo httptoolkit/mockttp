34d0a8b553c3ea9d7c029e73c004d85d76e6b4c6
Tim Perry <pimterry@gmail.com>
2019-05-15 13:06:08 +0200

    Fix incorrect Http(s)ProxyAgent types

--- custom-typings/http-proxy-agent.d.ts
+++ custom-typings/http-proxy-agent.d.ts
@@ -2,7 +2,7 @@ declare module 'http-proxy-agent' {
     import { Agent } from 'http';
 
     class HttpProxyAgent extends Agent {
-        constructor (uri: string | { protocol?: string; host?: string; hostname?: string; port?: string });
+        constructor (uri: string | { protocol?: string; host?: string; hostname?: string; port?: number });
     }
 
     export = HttpProxyAgent;
--- custom-typings/https-proxy-agent.d.ts
+++ custom-typings/https-proxy-agent.d.ts
@@ -2,7 +2,7 @@ declare module 'https-proxy-agent' {
     import { Agent } from 'https';
 
     class HttpsProxyAgent extends Agent {
-        constructor (uri: string | { protocol?: string; host?: string; hostname?: string; port?: string });
+        constructor (uri: string | { protocol?: string; host?: string; hostname?: string; port?: number });
     }
 
     export = HttpsProxyAgent;
