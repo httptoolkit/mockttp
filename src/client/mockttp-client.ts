be1039d9da59c42b65e6e3c6d16a01391214f082
Tim Perry <pimterry@gmail.com>
2019-05-17 18:00:13 +0200

    Fix bug that disables subscriptions when using an old standalone server

--- src/client/mockttp-client.ts
+++ src/client/mockttp-client.ts
@@ -255,8 +255,11 @@ export default class MockttpClient extends AbstractMockttp implements Mockttp {
     public on(event: SubscribableEvent, callback: (data: any) => void): Promise<void> {
         // Ignore unknown events
         if (!_.includes(SUBSCRIBABLE_EVENTS, event)) return Promise.resolve();
-        // Ignore subscription events, if not supported by the server
-        if (!this.typeHasField('Subscription', 'failedTlsRequest')) return Promise.resolve();
+        // Ignore TLS error events, if not supported by the server
+        if (
+            event === 'tlsClientError' &&
+            !this.typeHasField('Subscription', 'failedTlsRequest')
+        ) return Promise.resolve();
 
         const standaloneStreamServer = this.mockServerOptions.standaloneServerUrl.replace(/^http/, 'ws');
         const url = `${standaloneStreamServer}/server/${this.port}/subscription`;
