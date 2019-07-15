edb3343751e9e6e583d40fbbd5dc1a7a45637fa3
Tim Perry <pimterry@gmail.com>
2019-07-15 18:53:41 +0200

    Delete now-unnecessary https-proxy-agent types
    The package now has its own types built-in.

--- custom-typings/https-proxy-agent.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'https-proxy-agent' {
-    import { Agent } from 'https';
-
-    class HttpsProxyAgent extends Agent {
-        constructor (uri: string | { protocol?: string; host?: string; hostname?: string; port?: number });
-    }
-
-    export = HttpsProxyAgent;
-}
