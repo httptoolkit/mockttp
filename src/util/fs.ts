8efe252daef435fb52a8613b2ded99b23eadb171
Tim Perry <pimterry@gmail.com>
2021-08-10 21:14:01 +0200

    Fix type breakage due to @types/node changes

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -5,8 +5,8 @@
 import fs = require('fs');
 
 export function readFile(filename: string, encoding: null): Promise<Buffer>;
-export function readFile(filename: string, encoding: string): Promise<string>;
-export function readFile(filename: string, encoding: string | null): Promise<string | Buffer> {
+export function readFile(filename: string, encoding: BufferEncoding): Promise<string>;
+export function readFile(filename: string, encoding: BufferEncoding | null): Promise<string | Buffer> {
     return new Promise((resolve, reject) => {
         fs.readFile(filename, encoding, (err, contents: string | Buffer) => {
             if (err) reject(err);
