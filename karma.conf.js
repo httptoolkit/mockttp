85079f84a2bb7eb71772dafc9beb90dd70f337fd
Tim Perry <pimterry@gmail.com>
2022-04-29 18:10:54 +0200

    Update tests to Webpack 5 (v4 is unusable with Node 18)

--- karma.conf.js
+++ karma.conf.js
@@ -2,30 +2,57 @@ const tmp = require('tmp');
 tmp.setGracefulCleanup();
 
 const webpack = require('webpack');
+const outputDir = tmp.dirSync({ unsafeCleanup: true }).name;
 
 // Run a websocket server in the background for testing
 require('./test/fixtures/websocket-test-server');
 
 module.exports = function(config) {
     config.set({
-        frameworks: ['mocha', 'chai'],
+        frameworks: ['mocha', 'chai', 'webpack'],
         files: [
-            'test/**/*.spec.ts'
+            'test/**/*.spec.ts',
+            // Required for wasm due to https://github.com/ryanclark/karma-webpack/issues/498. Results
+            // in an annoying warning before the webpack build, but then it works fine.
+            { pattern: `${outputDir}/**/*`, included: false, served: true }
         ],
         mime: { 'text/x-typescript': ['ts'] },
         webpack: {
             mode: 'development',
-            devtool: false,
+            devtool: 'source-map',
             resolve: {
                 extensions: ['.ts', '.js'],
                 alias: {
                     // Here we put stubs for non-browser modules that are used by tests, not core code.
                     // Core code stubs are set in pkgJson.browser.
-                    "http-proxy-agent$": require.resolve('./test/empty-stub.js'),
-                    "https-proxy-agent$": require.resolve('./test/empty-stub.js'),
-                    "request-promise-native$": require.resolve('./test/empty-stub.js'),
-                    "fs-extra$": require.resolve('./test/empty-stub.js'),
-                    "portfinder$": require.resolve('./test/empty-stub.js')
+                    "http-proxy-agent": require.resolve('./test/empty-stub.js'),
+                    "https-proxy-agent": require.resolve('./test/empty-stub.js'),
+                    "request-promise-native": require.resolve('./test/empty-stub.js'),
+                    "fs-extra": require.resolve('./test/empty-stub.js'),
+                    "portfinder": require.resolve('./test/empty-stub.js'),
+                    "dns2": require.resolve('./test/empty-stub.js')
+                },
+                fallback: {
+                    // With Webpack 5, we need explicit mocks for all node modules. Because the
+                    // tests are the same for node & browser, with tests simply skipped in
+                    // browsers, plus some actual deps on node modules, we end up with a bunch
+                    // of deps that need to be manually included/skipped here:
+                    fs: false,
+                    net: false,
+                    http: false,
+                    https: false,
+                    http2: false,
+                    tls: false,
+
+                    assert: require.resolve('assert/'),
+                    buffer: require.resolve('buffer/'),
+                    crypto: require.resolve('crypto-browserify'),
+                    zlib: require.resolve('browserify-zlib'),
+                    stream: require.resolve('stream-browserify'),
+                    path: require.resolve('path-browserify'),
+                    querystring: require.resolve('querystring-es3'),
+                    util: require.resolve('util/'),
+                    url: require.resolve('url/')
                 }
             },
             module: {
@@ -33,21 +60,23 @@ module.exports = function(config) {
                     { test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }
                 ]
             },
+            experiments: {
+                asyncWebAssembly: true
+            },
             node: {
-                fs: 'empty',
                 __dirname: true
             },
             plugins: [
-                new webpack.IgnorePlugin(/^dns2$/),
-                new webpack.DefinePlugin({
-                    "process.version": '"' + process.version + '"'
-                }),
                 new webpack.SourceMapDevToolPlugin({
                     test: /\.(ts|js|css)($|\?)/i
+                }),
+                new webpack.ProvidePlugin({
+                    process: 'process/browser',
+                    Buffer: ['buffer', 'Buffer'],
                 })
             ],
             output: {
-                path: tmp.dirSync()
+                path: outputDir
             }
         },
         webpackMiddleware: {
