0f58b836033323000bb269934ea6397c5a912ded
Tim Perry <pimterry@gmail.com>
2023-03-15 12:26:49 +0100

    Add a test covering the existing flexible form matching behaviour

--- test/integration/matchers/form-matching.spec.ts
+++ test/integration/matchers/form-matching.spec.ts
@@ -26,6 +26,22 @@ describe("Form data matching", function () {
         })).to.have.responseText("matched");
     });
 
+    it("should match requests by form data flexibly, ignoring additional data", async () => {
+        let form = new URLSearchParams();
+
+        form.set('firstFields', 'hello');
+        form.set('shouldMatch', 'yes');
+        form.set('otherFields', 'goodbye');
+
+        return expect(fetch(server.url, {
+            method: 'POST',
+            headers: new Headers({
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }),
+            body: form
+        })).to.have.responseText("matched");
+    });
+
     it("shouldn't match requests with the wrong form data", async () => {
         let form = new URLSearchParams();
         form.set('shouldMatch', 'no');
