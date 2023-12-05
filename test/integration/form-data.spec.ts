733d3cc0e228ec7b05871a93f4522b9fc402a76f
exoego <ytatsuno.jp@gmail.com>
2023-12-06 07:45:40 +0900

    add failing test

---
+++ test/integration/form-data.spec.ts
@@ -0,0 +1,51 @@
+import { getLocal } from "../..";
+import { expect, fetch as utilFetch, nodeOnly } from "../test-utils";
+
+// workaround to use real fetch in node v18 and later
+const fetch = (typeof globalThis.fetch === 'undefined') ? utilFetch : globalThis.fetch;
+
+describe("FormData", () => {
+    let server = getLocal();
+
+    beforeEach(() => server.start());
+    afterEach(() => server.stop());
+
+    it("should parse application/x-www-form-urlencoded", async () => {
+        await server.forPost("/mocked-endpoint").thenCallback(async (req) => {
+            const formData = await req.body.getFormData() ;
+            return {
+                statusCode: 200,
+                body: JSON.stringify(formData),
+            };
+        });
+
+        await expect(
+            fetch(server.urlFor("/mocked-endpoint"), {
+                method: "POST",
+                body: "id=123&id=456&id=789&order=desc"
+            })
+        ).to.responseText("{\"id\":[\"123\",\"456\",\"789\"],\"order\":\"desc\"}");
+    });
+
+    it("should parse multipart/form-data", async () => {
+        await server.forPost("/mocked-endpoint").thenCallback(async (req) => {
+            const formData = await req.body.getFormData() ;
+            return {
+                statusCode: 200,
+                body: JSON.stringify(formData),
+            };
+        });
+
+        const formData = new FormData();
+        formData.append("id", "123");
+        formData.append("id", "456");
+        formData.append("id", "789");
+        formData.append("order", "desc");
+        await expect(
+            fetch(server.urlFor("/mocked-endpoint"), {
+                method: "POST",
+                body: formData,
+            })
+        ).to.responseText("{\"id\":[\"123\",\"456\",\"789\"],\"order\":\"desc\"}");
+    });
+});
