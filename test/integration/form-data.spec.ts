abf8d193903cae25444e6921a644c482ca707ff4
exoego <ytatsuno.jp@gmail.com>
2023-12-06 21:30:29 +0900

    Run test in Node.js 18+

--- test/integration/form-data.spec.ts
+++ test/integration/form-data.spec.ts
@@ -1,5 +1,6 @@
 import { getLocal } from "../..";
-import { expect, File, fetch as utilFetch } from "../test-utils";
+import {expect, File, fetch as utilFetch, NATIVE_FETCH_SUPPORTED} from "../test-utils";
+import semver = require("semver/preload");
 
 // workaround to use real fetch in node v18 and later
 const fetch = (typeof globalThis.fetch === 'undefined') ? utilFetch : globalThis.fetch;
@@ -10,42 +11,51 @@ describe("FormData", () => {
     beforeEach(() => server.start());
     afterEach(() => server.stop());
 
-    it("should parse application/x-www-form-urlencoded", async () => {
-        const endpoint = await server.forPost("/mocked-endpoint").thenReply(200);
+    describe("application/x-www-form-urlencoded", () => {
+        it("should parse application/x-www-form-urlencoded", async () => {
+            const endpoint = await server.forPost("/mocked-endpoint").thenReply(200);
 
-        await fetch(server.urlFor("/mocked-endpoint"), {
-            method: "POST",
-            body: "id=123&id=456&id=789&order=desc"
-        });
+            await fetch(server.urlFor("/mocked-endpoint"), {
+                method: "POST",
+                body: "id=123&id=456&id=789&order=desc"
+            });
 
-        const requests = await endpoint.getSeenRequests();
-        expect(requests.length).to.equal(1);
-        expect(await requests[0].body.getFormData()).to.deep.equal({
-            id: ["123", "456", "789"],
-            order: "desc",
+            const requests = await endpoint.getSeenRequests();
+            expect(requests.length).to.equal(1);
+            expect(await requests[0].body.getFormData()).to.deep.equal({
+                id: ["123", "456", "789"],
+                order: "desc",
+            });
         });
     });
 
-    it("should parse multipart/form-data", async () => {
-        const endpoint = await server.forPost("/mocked-endpoint").thenReply(200);
-
-        const formData = new FormData();
-        formData.append("id", "123");
-        formData.append("id", "456");
-        formData.append("id", "789");
-        formData.append("order", "desc");
-        formData.append("readme", new File(["file content"], "file.txt", { type: "text/plain" }));
-        await fetch(server.urlFor("/mocked-endpoint"), {
-            method: "POST",
-            body: formData,
+    describe("multipart/form-data", () => {
+        before(function () {
+            // Polyfill fetch encodes polyfill FormData into "[object FormData]", which is not parsable
+            if (!semver.satisfies(process.version, NATIVE_FETCH_SUPPORTED)) this.skip();
         });
 
-        const requests = await endpoint.getSeenRequests();
-        expect(requests.length).to.equal(1);
-        expect(await requests[0].body.getFormData()).to.deep.equal({
-            id: ["123", "456", "789"],
-            order: "desc",
-            readme: "file content",
+        it("should parse multipart/form-data", async () => {
+            const endpoint = await server.forPost("/mocked-endpoint").thenReply(200);
+
+            const formData = new FormData();
+            formData.append("id", "123");
+            formData.append("id", "456");
+            formData.append("id", "789");
+            formData.append("order", "desc");
+            formData.append("readme", new File(["file content"], "file.txt", {type: "text/plain"}));
+            await fetch(server.urlFor("/mocked-endpoint"), {
+                method: "POST",
+                body: formData,
+            });
+
+            const requests = await endpoint.getSeenRequests();
+            expect(requests.length).to.equal(1);
+            expect(await requests[0].body.getFormData()).to.deep.equal({
+                id: ["123", "456", "789"],
+                order: "desc",
+                readme: "file content",
+            });
         });
     });
 });
