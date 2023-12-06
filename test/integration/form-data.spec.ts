f3c1081f5e15e1f0713b5567d1ff79167b74a865
exoego <ytatsuno.jp@gmail.com>
2023-12-06 20:15:01 +0900

    Simplify mocking and assertion

--- test/integration/form-data.spec.ts
+++ test/integration/form-data.spec.ts
@@ -11,30 +11,23 @@ describe("FormData", () => {
     afterEach(() => server.stop());
 
     it("should parse application/x-www-form-urlencoded", async () => {
-        await server.forPost("/mocked-endpoint").thenCallback(async (req) => {
-            const formData = await req.body.getFormData() ;
-            return {
-                statusCode: 200,
-                body: JSON.stringify(formData),
-            };
+        const endpoint = await server.forPost("/mocked-endpoint").thenReply(200);
+
+        await fetch(server.urlFor("/mocked-endpoint"), {
+            method: "POST",
+            body: "id=123&id=456&id=789&order=desc"
         });
 
-        await expect(
-            fetch(server.urlFor("/mocked-endpoint"), {
-                method: "POST",
-                body: "id=123&id=456&id=789&order=desc"
-            })
-        ).to.responseText("{\"id\":[\"123\",\"456\",\"789\"],\"order\":\"desc\"}");
+        const requests = await endpoint.getSeenRequests();
+        expect(requests.length).to.equal(1);
+        expect(await requests[0].body.getFormData()).to.deep.equal({
+            id: ["123", "456", "789"],
+            order: "desc",
+        });
     });
 
     it("should parse multipart/form-data", async () => {
-        await server.forPost("/mocked-endpoint").thenCallback(async (req) => {
-            const formData = await req.body.getFormData() ;
-            return {
-                statusCode: 200,
-                body: JSON.stringify(formData),
-            };
-        });
+        const endpoint = await server.forPost("/mocked-endpoint").thenReply(200);
 
         const formData = new FormData();
         formData.append("id", "123");
@@ -42,11 +35,17 @@ describe("FormData", () => {
         formData.append("id", "789");
         formData.append("order", "desc");
         formData.append("readme", new File(["file content"], "file.txt", { type: "text/plain" }));
-        await expect(
-            fetch(server.urlFor("/mocked-endpoint"), {
-                method: "POST",
-                body: formData,
-            })
-        ).to.responseText("{\"id\":[\"123\",\"456\",\"789\"],\"order\":\"desc\",\"readme\":\"file content\"}");
+        await fetch(server.urlFor("/mocked-endpoint"), {
+            method: "POST",
+            body: formData,
+        });
+
+        const requests = await endpoint.getSeenRequests();
+        expect(requests.length).to.equal(1);
+        expect(await requests[0].body.getFormData()).to.deep.equal({
+            id: ["123", "456", "789"],
+            order: "desc",
+            readme: "file content",
+        });
     });
 });
